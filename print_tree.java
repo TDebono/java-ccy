b1dc8f426066c9a4cbd104d6fb09d4186459f8be
Timo Debono <59320688+TDebono@users.noreply.github.com>
2021-06-22 00:18:58 +0200

    Create print_tree.java

---
+++ print_tree.java
@@ -0,0 +1,13 @@
+// Define your tree class in this file!
+
+public class Tree {
+
+  public static void main (String[] args) {
+    // print tree
+    System.out.println("  *  ");
+    System.out.println(" *** ");
+    System.out.println(" *** ");
+    System.out.println("  *  ");
+    System.out.println("  *  ");  
+  }
+}
