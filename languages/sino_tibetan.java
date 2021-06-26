3eacba1ae619bc7602efc8a7d25cca455e0030a6
Timo Debono <59320688+TDebono@users.noreply.github.com>
2021-06-26 09:23:36 +0200

    Create sino_tibetan.java

---
+++ languages/sino_tibetan.java
@@ -0,0 +1,12 @@
+public class SinoTibetan extends Language {
+
+  public SinoTibetan(String langName, int speakers) {
+    super(langName, speakers, "Asia", "subject-object-verb");
+
+    if (langName.contains("Chinese")) {
+      this.wordOrder = "subject-verb-object";
+    }
+  }
+
+
+}
